0f26276026744b18515b578494185e50534be4d6
Megan Starkey <143949698+meganstarkey4@users.noreply.github.com>
2023-12-10 17:23:43 -0700

    fixed

--- js-final/js-final.js
+++ js-final/js-final.js
@@ -2,9 +2,16 @@
   const form = document.getElementById('quizForm');
   const formData = new FormData(form);
   const submitButton = document.getElementById("submitBtn")
+  const answersMap = {
+    q1: "megan",
+    q2: "grasshopper",
+    q3: "doodle",
+    q4: "waffles",
+    q5: "allergy"
+  }
   submitButton.addEventListener("click",()=> {
-    if (form.q1.value == answersMap[q1] && form.q2.value == answersMap[q2] && form.q3.value == 
-        answersMap[q3] && form.q4.value == answersMap[q4] && form.q5.value == answersMap[q5]) {
+    if (form.q1.value == answersMap['q1'] && form.q2.value == answersMap['q2'] && form.q3.value == 
+        answersMap['q3'] && form.q4.value == answersMap['q4'] && form.q5.value == answersMap['q5']) {
         alert("Nice phone number! It's " + form.num1.value +form.num2.value+form.num3.value+
         form.num4.value+form.num5.value+form.num5.value+form.num6.value+form.num7.value+
         form.num8.value+form.num9.value+form.num10.value)
@@ -13,11 +20,3 @@
     }
   }
   )
-
-  const answersMap = {
-    q1: "megan",
-    q2: "grasshopper",
-    q3: "doodle",
-    q4: "waffles",
-    q5: "allergy"
-  }
