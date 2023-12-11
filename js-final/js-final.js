ab637a3ece1030acc3b4a98c3ea409a2e1c286a5
Megan Starkey <143949698+meganstarkey4@users.noreply.github.com>
2023-12-10 17:30:25 -0700

    Update js-final.js

--- js-final/js-final.js
+++ js-final/js-final.js
@@ -13,7 +13,7 @@
     if (form.q1.value == answersMap['q1'] && form.q2.value == answersMap['q2'] && form.q3.value == 
         answersMap['q3'] && form.q4.value == answersMap['q4'] && form.q5.value == answersMap['q5']) {
         alert("Nice phone number! It's " + form.num1.value +form.num2.value+form.num3.value+
-        form.num4.value+form.num5.value+form.num5.value+form.num6.value+form.num7.value+
+        form.num4.value+form.num5.value+form.num6.value+form.num7.value+
         form.num8.value+form.num9.value+form.num10.value)
     } else {
         alert("Wrong, you don't know me and now I am crying")
